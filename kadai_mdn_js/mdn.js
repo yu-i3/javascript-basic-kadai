7e4778771e89bfba9fa26e708dc49b8fd5ccbb77
yu-i3 <your.happiness03@gmail.com>
2026-04-23 23:02:03 +0900

    Fix mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,10 +1,12 @@
 // Dateクラスのインスタンス生成
-const date = new Date();
+const today = new Date();
 
-// 年月日を設定
-date.setFullYear(2024);
-date.setMonth(10);
-date.setDate(12);
+// 年を取得
+const year = today.getFullYear();
+// 月を取得
+const month = today.getMonth() + 1;
+// 日を取得
+const day = today.getDate();
 
-// 年月日を取得し、出力する
-console.log(date.getFullYear() + '年' + date.getMonth() + '月' + date.getDate() + '日');
+// 年月日を出力する
+console.log(year + '年' + month + '月' + day + '日');
